8ee2dc1dba9cfddcf91861687d9de5bb0dc7ee0a
Loryn Mason <masonloryn@gmail.com>
2018-12-28 19:13:44 -0700

    Convert message back to functional, get message to disapear after 10sec

--- src/containers/Message/Message.js
+++ src/containers/Message/Message.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { addMessage } from '../../actions'
 
-export class Message extends Component {
-  render() {
-    if(this.props.message) {
-      return (
-        <div className="message-container">
-          <p>
-            {this.props.message}
-          </p>
-        </div>
-      )
-    }
-    return null
+export const Message = (props) => {
+  setTimeout(() => {
+    props.addMess(null)
+  }, 1000);
+
+  if(props.message) {
+    return (
+      <div className="message-container">
+        <p>
+          {props.message}
+        </p>
+      </div>
+    )
   }
+  return null
 }
-
 
 export const mapStateToProps = (state) => ({
   message: state.message
 })
 
+export const mapDispatchToProps = (dispatch) => ({
+  addMess: message => dispatch(addMessage(message))
+})
+
 export default connect(
-  mapStateToProps
+  mapStateToProps, mapDispatchToProps
 )(Message);
